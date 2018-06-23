0daf69fdfe09e51d4f3e0739f2e722c416e6c3f8
maeharin <maeharin@gmail.com>
2018-06-23 17:07:59 +0900

    use library 0.0.1

---
+++ src/main/kotlin/com/maeharin/hello-gradle-plugin-sample/HelloGradePluginSample.kt
@@ -0,0 +1,10 @@
+package com.maeharin.`hello-gradle-plugin-sample`
+
+import com.maeharin.hello_gradle_plugin.HelloGradleCore
+
+class HelloGradePluginSample {
+    fun main() {
+        val core = HelloGradleCore()
+        core.hello()
+    }
+}
